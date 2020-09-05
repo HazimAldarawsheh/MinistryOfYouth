24e2f24e3b5c5b699753a34a43489c9c4e898227
HazimAldarawsheh <Hazim.aldarawsheh@htu.edu.jo>
2020-09-05 19:13:09 +0300

    nav update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,10 +5,15 @@ var $test = $('.test');
 $sameer.waypoint(function(dir) {
     if (dir == 'down') {
         $test.addClass('sticky');
+        $test.addClass('navAn');
+        $test.removeClass('cool-link');
         console.log('down');
     } else {
         $test.removeClass('sticky');
         console.log('up');
+        $test.removeClass('navAn')
     }
 
 }, { offset: '30px' });
+
+
