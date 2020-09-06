b52e46b6b6acaab29e4cf670cdc29d7b90f2ed93
HazimAldarawsheh <Hazim.aldarawsheh@htu.edu.jo>
2020-09-06 14:39:45 +0300

    majorUpdate

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,11 +7,11 @@ $sameer.waypoint(function(dir) {
     if (dir == 'down') {
         $test.addClass('sticky');
         $test.addClass('navAn');
-        $topright.removeClass('cool-link');
+        $topright.removeClass('cool-linkN');
     } else {
         $test.removeClass('sticky');
         $test.removeClass('navAn');
-        $topright.addClass('cool-link');
+        $topright.addClass('cool-linkN');
     }
 
 }, { offset: '30px' });
