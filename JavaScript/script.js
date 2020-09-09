c4861085c32d127940036fed5a82f409f5b7847f
HazimAldarawsheh <Hazim.aldarawsheh@htu.edu.jo>
2020-09-09 16:20:48 +0300

    shit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,38 @@
 var $sameer =$('.js--body');
-
-var $test = $('.test');
+var $burger =$('.burger-menu');
+var $navjq = $('.nav-jq');
 var $topright=$('.top-right')
 
 $sameer.waypoint(function(dir) {
     if (dir == 'down') {
-        $test.addClass('sticky');
-        $test.addClass('navAn');
+        $navjq.addClass('sticky');
+        $navjq.addClass('navAn');
         $topright.removeClass('cool-linkN');
+        $burger.removeClass('burger');
+        $burger.removeClass('shadow-5');
+
     } else {
-        $test.removeClass('sticky');
-        $test.removeClass('navAn');
+        $navjq.removeClass('sticky');
+        $navjq.removeClass('navAn');
         $topright.addClass('cool-linkN');
+        $burger.addClass('burger');
+        $burger.addClass('shadow-5');
+
+
     }
 
 }, { offset: '100px' });
 
 
+
+
+
+function burgerMenu() {
+    var menu = document.getElementById('burgerDis');
+    if (menu.style.display === "block") {
+     menu.style.display = "none";
+    } else {
+     menu.style.display = "block";
+     menu.classList.add('topnav');
+    }
+  }
